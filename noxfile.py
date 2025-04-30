fbfb0d47c6b2119d43c21e3b6345f0b57997554b
shenxianpeng <xianpeng.shen@gmail.com>
2025-04-30 20:48:45 +0300

    add .pre-commit-config.yaml

--- noxfile.py
+++ noxfile.py
@@ -5,6 +5,6 @@ nox.options.reuse_existing_virtualenvs = True
 @nox.session
 def tests(session):
     """Run benchmarks."""
-    session.install("-r", "requirements.txt")
+    session.install("pytest", "pytest-benchmark")
     session.run("pytest", "benchmarks/test_pip_install.py", "--benchmark-autosave")
     session.run("pytest-benchmark", "compare")
